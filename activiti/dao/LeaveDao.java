fa1b6be4d16492ddb6f50a26702d9c4797b0a056
lithium429 <lithium429@163.com>
2015-07-23 11:54:31 +0800

    test mod

--- activiti/dao/LeaveDao.java
+++ activiti/dao/LeaveDao.java
@@ -6,11 +6,7 @@ import org.springframework.stereotype.Component;
 
 import com.xz.project.activiti.pojo.oa.Leave;
 
-/**
- * 请假实体管理接口
- *
- * @author HenryYan
- */
+
 @Component
 public interface LeaveDao extends CrudRepository<Leave, Long> {
 }
